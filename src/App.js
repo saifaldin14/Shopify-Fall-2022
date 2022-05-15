a39036f10fd430eef0c7149aa7b212a37e1d9738
saifaldin14 <saifaldindeveloper@gmail.com>
2022-05-15 12:39:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { postGPTAction } from "./actions/postGPTData";
+import { connect } from "react-redux";
+import FeedComponent from "./components/FeedComponent";
+import DatePickerComponent from "./components/DatePickerComponent";
+import Lottie from "react-lottie";
+import { default as animationData } from "./assets/lotties/loading.json";
 
-function App() {
+const App = (props) => {
+  /**
+   * Function to call the prop that will dispatch an action to fetch data from the API
+   * to retrieve data between startDate and endDate
+   * @param {object} data
+   */
+  const onPostGPTData = (data) => {
+    props.postGPTData(data);
+  };
+
+  //The default configurations for the loading animation
+  //Loop until props.isLoading is false
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Space-tagram!</h1>
+      {!props.isLoading ? (
+        <>
+          <DatePickerComponent onGetNasaData={onGetNasaData} />
+          <FeedComponent nasaData={props.nasaData.data} />
+        </>
+      ) : (
+        <Lottie options={defaultOptions} height={400} width={400} />
+      )}
     </div>
   );
+};
+
+function mapStateToProps(state) {
+  const gptData = state.postGPTData;
+  const isLoading = state.postGPTData.isLoading;
+
+  return {
+    gptData,
+    isLoading,
+  };
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    postGPTData: (data) => dispatch(postGPTAction(data)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
